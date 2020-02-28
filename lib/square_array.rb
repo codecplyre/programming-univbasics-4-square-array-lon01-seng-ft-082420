cf2d9022dbc3843e95abc64f1f89da7c6cf1bcca
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-02-28 21:48:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do 
-    array[counter].to_i ** 2
+    new_array << array[counter].to_i ** 2
     counter += 1 
   end
-    array
+  new_array
 end
